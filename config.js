0505c5da95ecee1fff8ca41d871edfb293a4bacb
_Frutaz_ <terentevsemen20052013@gmail.com>
2026-07-30 15:55:30 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://ВАШ-ПРОЕКТ.supabase.co";
+const SUPABASE_ANON_KEY = "ВАШ_ANON_KEY";
+ 
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
